94cd8da505ffb833e07efc61cf5e072c46540bd8
zhaomingliu <forsaya@gmail.com>
2018-07-24 09:38:43 +0800

    代码格式

--- src/main/java/com/qianmi/analysis/sub/SubStringTokenizer.java
+++ src/main/java/com/qianmi/analysis/sub/SubStringTokenizer.java
@@ -2,7 +2,6 @@ package com.qianmi.analysis.sub;
 
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.analysis.util.CharacterUtils;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -16,11 +15,7 @@ public class SubStringTokenizer extends Tokenizer {
 
     private static final int IO_BUFFER_SIZE = 4096;
 
-    private final CharacterUtils charUtils;
-
-    private final CharacterUtils.CharacterBuffer ioBuffer = CharacterUtils.newCharacterBuffer(IO_BUFFER_SIZE);
-
-    private  char[] charArray = new char[IO_BUFFER_SIZE];
+    private char[] charArray = new char[IO_BUFFER_SIZE];
 
     private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
 
@@ -40,7 +35,6 @@ public class SubStringTokenizer extends Tokenizer {
 
     public SubStringTokenizer(Reader input, List<Position> positionList) {
         super(input);
-        charUtils = CharacterUtils.getInstance();
         this.positions = positionList;
     }
 
@@ -75,21 +69,14 @@ public class SubStringTokenizer extends Tokenizer {
         }
 
         Position position = positions.get(offset);
-//        charUtils.fill(ioBuffer, input);
         int byteSize = input.read(charArray);
-        int validSize = position.getSize();
-        if (byteSize < position.getSize()) {
-            validSize = ioBuffer.getLength();
-        }
-
-//        char[] inputChars = ioBuffer.getBuffer();
-        char[] inputChars = charArray;
+        int validSize = Math.min(byteSize, position.getSize());
         char[] buffer = termAtt.buffer();
         if (Position.DIRECTION_PRE.equals(position.getDirection())) {
-            System.arraycopy(inputChars, 0, buffer, 0, validSize);
+            System.arraycopy(charArray, 0, buffer, 0, validSize);
         } else {
             int startPos = byteSize - validSize;
-            System.arraycopy(inputChars, startPos, buffer, 0, validSize);
+            System.arraycopy(charArray, startPos, buffer, 0, validSize);
         }
 
         termAtt.setLength(validSize);
@@ -101,6 +88,5 @@ public class SubStringTokenizer extends Tokenizer {
     public void reset() throws IOException {
         super.reset();
         offset = 0;
-//        ioBuffer.reset();
     }
 }
