6dc4f4827681f6a9e5789dd70a31a5d85ad8266c
侯锐 <hourui@qianmi.com>
2020-04-30 16:50:06 +0800

    添加分词属性参数

--- src/main/java/com/qianmi/elasticsearch/index/analysis/tokenizer/QianmiStandardTokenizer.java
+++ src/main/java/com/qianmi/elasticsearch/index/analysis/tokenizer/QianmiStandardTokenizer.java
@@ -3,6 +3,10 @@ package com.qianmi.elasticsearch.index.analysis.tokenizer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.analysis.tokenattributes.PositionIncrementAttribute;
+import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 
 import java.io.IOException;
 
@@ -12,6 +16,14 @@ import java.io.IOException;
 public class QianmiStandardTokenizer extends Tokenizer {
 
     private static final Logger LOG = LogManager.getLogger();
+    // 当前词
+    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    // 偏移量
+    private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+    // 距离
+    private final PositionIncrementAttribute positionAttr = addAttribute(PositionIncrementAttribute.class);
+    // 分词词性
+    private final TypeAttribute typeAtt = addAttribute(TypeAttribute.class);
 
     public QianmiStandardTokenizer() {
         LOG.info("Init class QianmiStandardTokenizer");
