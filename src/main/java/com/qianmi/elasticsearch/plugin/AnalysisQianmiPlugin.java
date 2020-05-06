8aa10f96b2f919a3dae323cba7fdff0d721dfaa1
侯锐 <hourui@qianmi.com>
2020-05-06 14:49:15 +0800

    移除与lambda表达式相关的注释

--- src/main/java/com/qianmi/elasticsearch/plugin/AnalysisQianmiPlugin.java
+++ src/main/java/com/qianmi/elasticsearch/plugin/AnalysisQianmiPlugin.java
@@ -21,30 +21,6 @@ public class AnalysisQianmiPlugin extends Plugin implements AnalysisPlugin {
     public Map<String, AnalysisModule.AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> getAnalyzers() {
         Map<String, AnalysisModule.AnalysisProvider<AnalyzerProvider<? extends Analyzer>>> extra = new HashMap<>();
 
-        /*
-         * 1. 这里需要一个实现了接口AnalysisModule.AnalysisProvider的类的对象；
-         * 2. AnalysisModule.AnalysisProvider接口有一个get方法为虚拟方法，实现该接口的类需要实现该方法；
-         * 3. 我们并不需要真的去实现一个类并且让该类实现这个get方法，而是可以使用匿名内部类的方式实现；
-         *    3.1 当然也可以实现一个类，让该类实现AnalysisModule.AnalysisProvider接口；
-         *    3.2 该类也需要实现get方法，get方法的逻辑还是一样的；
-         *    3.3 之后在这里创建一个该类的对象即可；
-         * 4. 如果使用匿名内部类，则只需要实现get方法即可：
-         *    new AnalysisModule.AnalysisProvider<AnalyzerProvider<? extends Analyzer>>() {
-         *        @Override
-         *        public AnalyzerProvider<? extends Analyzer> get(IndexSettings indexSettings, Environment environment, String name, Settings settings) throws IOException {
-         *            return new QianmiSubAnalyzerProvider(indexSettings, environment, name, settings);
-         *        }
-         *    }
-         *
-         * 5. 由于只需要实现一个get方法，所以可以使用lambda表达式对如上的代码优化如下：
-         *    (indexSettings, env, name, settings) -> {
-         *        return  new QianmiSubAnalyzerProvider(indexSettings, env, name, settings);
-         *    }
-         * 6. 由于该方法的方法体只有一行，所以可以把上面的表达式进一步的优化为如下的lambda表达式：
-         *    (indexSettings, env, name, settings) -> new QianmiSubAnalyzerProvider(indexSettings, env, name, settings)
-         * 7. 由于get方法的参数和后面创建对象的参数一致，所以可以使用lambda表达式进行进一步的优化：
-         *    QianmiSubAnalyzerProvider::new
-         */
         extra.put("qm_standard", QianmiStandardAnalyzerProvider::new);
         extra.put("sub", QianmiSubAnalyzerProvider::new);
 
